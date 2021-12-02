c0084deb624f820dab77f8284336fca33ba31bf6
Niayz <johndoe@example.com>
2021-12-02 14:23:26 +0300

    stepik file

---
+++ stepik2.3_step6.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+url = "http://suninjuly.github.io/redirect_accept.html"
+driver = webdriver.Chrome()
+driver.get(url)
+
+
+try:
+
+    driver.find_element_by_css_selector('.btn-primary').click()
+    time.sleep(2)
+    driver.switch_to.window(driver.window_handles[1])
+
+    x_value = driver.find_element_by_css_selector('#input_value')
+    x = x_value.text
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    y = calc(x)
+
+    driver.find_element_by_css_selector('#answer').send_keys(y)
+    driver.implicitly_wait(5)
+    driver.find_element_by_css_selector('.btn-primary').click()
+
+
+except Exception as ex:
+    print(ex)
+
+finally:
+    time.sleep(2)
+    driver.close()
+    driver.quit()
